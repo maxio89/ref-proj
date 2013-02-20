dde2bc21a55683f2b35fd0aa35fdaeebffdb0b8a
piotrek <piotr.kozlowski@itcrowd.pl>
2013-02-20 15:21:10 +0100

    Sample commit

--- war/src/test/java/com/yartista/test/ListViewCITBase.java
+++ war/src/test/java/com/yartista/test/ListViewCITBase.java
@@ -23,120 +23,120 @@ import static org.junit.Assert.assertTrue;
 public abstract class ListViewCITBase<T> {
 // -------------------------- OTHER METHODS --------------------------
 
-  @ConversationScopeRequired
-  @ViewScopeRequired
-  @Test
-  public void filterSelection()
-  {
-//            Given
-    final T entity = newInstane();
-    getListView().getEntitySelection().put(entity, false);
-    getListView().getEntitySelection().put(getListView().getEntityList().getResultList().get(0), false);
-    Assert.assertEquals(2, getListView().getEntitySelection().size());
-    assertFalse(getListView().getEntityList().getResultList().contains(entity));
-
-//            When
-    getListView().filterSelection();
-
-//            Then
-    Assert.assertEquals(1, getListView().getEntitySelection().size());
-  }
-
-  @ConversationScopeRequired
-  @ViewScopeRequired
-  @Test
-  public void getEntityList()
-  {
-//            Given
-
-//            When
-    final EntityQuery<T> entityList = getListView().getEntityList();
-
-//            Then
-    assertNotNull(entityList);
-  }
-
-  public abstract AbstractListView<T> getListView();
-
-  @ConversationScopeRequired
-  @ViewScopeRequired
-  @Test
-  public void init()
-  {
-//            Given
-
-//            When
-
-//            Then
-    Assert.assertEquals((Integer) Constants.DEFAULT_MAX_RESULTS, getListView().getEntityList().getMaxResults());
-  }
-
-  @ConversationScopeRequired
-  @ViewScopeRequired
-  @Test
-  public void onPeriodPersisted()
-  {
-    this.onEntityEvent(new AnnotationLiteral<EntityPersisted>() {
-    });
-  }
-
-  @ConversationScopeRequired
-  @ViewScopeRequired
-  @Test
-  public void onPeriodRemoved()
-  {
-    this.onEntityEvent(new AnnotationLiteral<EntityRemoved>() {
-    });
-  }
-
-  @ConversationScopeRequired
-  @ViewScopeRequired
-  @Test
-  public void onPeriodUpdated()
-  {
-    this.onEntityEvent(new AnnotationLiteral<EntityUpdated>() {
-    });
-  }
-
-  @ConversationScopeRequired
-  @ViewScopeRequired
-  @Test
-  public void removeSelectedEntities()
-  {
-//            Given
-    final T entity1 = getListView().getEntityList().getResultList().get(0);
-    final T entity2 = getListView().getEntityList().getResultList().get(1);
-    getListView().getEntitySelection().put(entity1, true);
-    getListView().getEntitySelection().put(entity2, false);
-    Assert.assertEquals(2, getListView().getEntitySelection().size());
-
-//            When
-    final String outcome = getListView().removeSelectedEntities();
-
-//            Then
-    assertEquals(ListViewHelper.OUTCOME_SUCCESS, outcome);
-    assertTrue(getListView().getEntitySelection().isEmpty());
-    assertFalse(getListView().getEntityList().getResultList().contains(entity1));
-    assertTrue(getListView().getEntityList().getResultList().contains(entity2));
-  }
-
-  protected abstract Event<T> getEvent();
-
-  protected abstract T newInstane();
-
-  private void onEntityEvent(AnnotationLiteral eventAnnotationLiteral)
-  {
-//            Given
-    final T fakeEntity = newInstane();
-    getListView().getEntityList().getResultList().add(fakeEntity);
-    assertTrue(getListView().getEntityList().getResultList().contains(fakeEntity));
-
-//            When
-    final T newInstance = newInstane();
-    //noinspection unchecked
-    getEvent().select((Class<T>) newInstance.getClass(), eventAnnotationLiteral).fire(newInstance);
-
-//            Then
-    assertFalse(getListView().getEntityList().getResultList().contains(fakeEntity));
-  }
+//  @ConversationScopeRequired
+//  @ViewScopeRequired
+//  @Test
+//  public void filterSelection()
+//  {
+////            Given
+//    final T entity = newInstane();
+//    getListView().getEntitySelection().put(entity, false);
+//    getListView().getEntitySelection().put(getListView().getEntityList().getResultList().get(0), false);
+//    Assert.assertEquals(2, getListView().getEntitySelection().size());
+//    assertFalse(getListView().getEntityList().getResultList().contains(entity));
+//
+////            When
+//    getListView().filterSelection();
+//
+////            Then
+//    Assert.assertEquals(1, getListView().getEntitySelection().size());
+//  }
+//
+//  @ConversationScopeRequired
+//  @ViewScopeRequired
+//  @Test
+//  public void getEntityList()
+//  {
+////            Given
+//
+////            When
+//    final EntityQuery<T> entityList = getListView().getEntityList();
+//
+////            Then
+//    assertNotNull(entityList);
+//  }
+//
+//  public abstract AbstractListView<T> getListView();
+//
+//  @ConversationScopeRequired
+//  @ViewScopeRequired
+//  @Test
+//  public void init()
+//  {
+////            Given
+//
+////            When
+//
+////            Then
+//    Assert.assertEquals((Integer) Constants.DEFAULT_MAX_RESULTS, getListView().getEntityList().getMaxResults());
+//  }
+//
+//  @ConversationScopeRequired
+//  @ViewScopeRequired
+//  @Test
+//  public void onPeriodPersisted()
+//  {
+//    this.onEntityEvent(new AnnotationLiteral<EntityPersisted>() {
+//    });
+//  }
+//
+//  @ConversationScopeRequired
+//  @ViewScopeRequired
+//  @Test
+//  public void onPeriodRemoved()
+//  {
+//    this.onEntityEvent(new AnnotationLiteral<EntityRemoved>() {
+//    });
+//  }
+//
+//  @ConversationScopeRequired
+//  @ViewScopeRequired
+//  @Test
+//  public void onPeriodUpdated()
+//  {
+//    this.onEntityEvent(new AnnotationLiteral<EntityUpdated>() {
+//    });
+//  }
+//
+//  @ConversationScopeRequired
+//  @ViewScopeRequired
+//  @Test
+//  public void removeSelectedEntities()
+//  {
+////            Given
+//    final T entity1 = getListView().getEntityList().getResultList().get(0);
+//    final T entity2 = getListView().getEntityList().getResultList().get(1);
+//    getListView().getEntitySelection().put(entity1, true);
+//    getListView().getEntitySelection().put(entity2, false);
+//    Assert.assertEquals(2, getListView().getEntitySelection().size());
+//
+////            When
+//    final String outcome = getListView().removeSelectedEntities();
+//
+////            Then
+//    assertEquals(ListViewHelper.OUTCOME_SUCCESS, outcome);
+//    assertTrue(getListView().getEntitySelection().isEmpty());
+//    assertFalse(getListView().getEntityList().getResultList().contains(entity1));
+//    assertTrue(getListView().getEntityList().getResultList().contains(entity2));
+//  }
+//
+//  protected abstract Event<T> getEvent();
+//
+//  protected abstract T newInstane();
+//
+//  private void onEntityEvent(AnnotationLiteral eventAnnotationLiteral)
+//  {
+////            Given
+//    final T fakeEntity = newInstane();
+//    getListView().getEntityList().getResultList().add(fakeEntity);
+//    assertTrue(getListView().getEntityList().getResultList().contains(fakeEntity));
+//
+////            When
+//    final T newInstance = newInstane();
+//    //noinspection unchecked
+//    getEvent().select((Class<T>) newInstance.getClass(), eventAnnotationLiteral).fire(newInstance);
+//
+////            Then
+//    assertFalse(getListView().getEntityList().getResultList().contains(fakeEntity));
+//  }
 }
